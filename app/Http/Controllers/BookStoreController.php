cb248e207787213ee47412b001c915584bb0c8ac
Jeferson Silva <jeferson.i.silva@gmail.com>
2022-11-15 11:45:01 -0300

    Create BookStore model and controller

---
+++ app/Http/Controllers/BookStoreController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BookStore;
+use Illuminate\Http\Request;
+
+class BookStoreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\BookStore  $bookStore
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BookStore $bookStore)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BookStore  $bookStore
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BookStore $bookStore)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\BookStore  $bookStore
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BookStore $bookStore)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BookStore  $bookStore
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BookStore $bookStore)
+    {
+        //
+    }
+}
